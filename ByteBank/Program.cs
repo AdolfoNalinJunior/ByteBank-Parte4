769baa3d3f72e53fe5ad19ca99eef99ed362995d
AdolfoNalinJunior <adolfonalin22@outlook.com>
2022-07-05 20:34:33 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodo();
+        }
+
+        private static void Metodo()
+        {
+            TestaDivisao(10);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
+        }
+    }
+}
