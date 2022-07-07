d35d33db0bc5e62a0c94922e1fb42b5db34b11c2
AdolfoNalinJunior <adolfonalin22@outlook.com>
2022-07-07 20:20:00 -0300

    Como que funcioana o throw
    throw é uma saída de fluxo, sendo assim, quando o método precisa de um return, é usado o therow para sair da manipulação do código.
    
    Isso se manipulação de tratamento for realizada dentro de método.

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -41,10 +41,16 @@ namespace ByteBank
 
         private static int Dividir(int numero, int divisor)
         {
-            // Erro de referência
-            ContaCorrente conta = null;
-            Console.WriteLine(conta.Saldo);
-            return numero / divisor;
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exeção com número = " + numero + " e divisor = " + divisor);
+                throw;
+            }
+            
         }
     }
 }
